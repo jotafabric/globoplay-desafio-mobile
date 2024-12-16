83cf286288694628b015cc51642e96b25009dcd7
jotafabric <contato@josiasfabricio.combr>
2024-12-16 17:57:58 -0300

    Initial files

---
+++ ChallengeGloboplay/ChallengeGloboplay/ChallengeGloboplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeGloboplayApp.swift
+//  ChallengeGloboplay
+//
+//  Created by Josias Fabrício on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeGloboplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChallengeGloboplay/ChallengeGloboplayTests/ChallengeGloboplayTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChallengeGloboplayTests.swift
+//  ChallengeGloboplayTests
+//
+//  Created by Josias Fabrício on 16/12/24.
+//
+
+import Testing
+
+struct ChallengeGloboplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
